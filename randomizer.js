a846b0e8c05e84c3d4b05ea0c454a6a9eb3c2742
Valerian Mahdi Pratama <yakira979@gmail.com>
2020-11-09 00:53:28 +0700

    Add files via upload

---
+++ randomizer.js
@@ -0,0 +1,8 @@
+var promptList = [
+    "To avoid copyright issue",
+    "Please input your prompt manually",
+    "Since I basically just copy-paste prompt from online resource",
+    "Write it as an array like this"
+]
+
+var randomItem = promptList[Math.floor(Math.random()*promptList.length)];
